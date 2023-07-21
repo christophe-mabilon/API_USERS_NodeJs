7c328bb5c31d3905443c33bb2cb5f9e7848d82f1
Bertrand2808 <bertrand2808@icloud.com>
2023-07-21 15:25:24 +0200

    continue work on swagger

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -34,10 +34,6 @@ const userRouter = express.Router();
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/User'
- *       401:
- *         description: Not authorized, token missing or invalid.
- *       500:
- *         description: Error, cannot get users' information.
  */
 userRouter.get("/super-admin/users", [authJwt.verifyToken, authJwt.isSuperAdmin], getAllUsersInfosSuperAdminAccess);
 
@@ -47,8 +43,6 @@ userRouter.get("/super-admin/users", [authJwt.verifyToken, authJwt.isSuperAdmin]
  *   get:
  *     summary: Get all users' information for admin access only.
  *     tags: [Users]
- *     security:
- *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: Success, returns all admin users' information.
@@ -58,10 +52,6 @@ userRouter.get("/super-admin/users", [authJwt.verifyToken, authJwt.isSuperAdmin]
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/User'
- *       401:
- *         description: Not authorized, token missing or invalid.
- *       500:
- *         description: Error, cannot get users' information.
  */
 userRouter.get("/admin/users", [authJwt.verifyToken, authJwt.isAdmin], getAllUsersInfos);
 
@@ -71,22 +61,39 @@ userRouter.get("/admin/users", [authJwt.verifyToken, authJwt.isAdmin], getAllUse
  *   get:
  *     summary: Get user information by ID.
  *     tags: [Users]
- *     security:
- *       - bearerAuth: []
  *     parameters:
- *       - name: id
- *         in: path
+ *       - in: path
+ *         name: id
  *         required: true
  *         description: User ID to fetch.
  *         schema:
  *           type: string
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: Success, returns user information.
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/User'.
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: User ID.
+ *                   example: 5f9d4f8f5f9d4f8f5f9d4f8f
+ *                 username:
+ *                   type: string
+ *                   description: User username.
+ *                   example: bertrand_test
+ *                 email:
+ *                   type: string
+ *                   description: User email.
+ *                   example: bertrand@test.fr
+ *                 roles:
+ *                   type: array
+ *                   description: User roles.
+ *                   example: ["user", "admin"]
  */
 userRouter.get("/user/:id", [authJwt.verifyToken], getUserInfos);
 
@@ -96,21 +103,19 @@ userRouter.get("/user/:id", [authJwt.verifyToken], getUserInfos);
  *   put:
  *     summary: Edit user information.
  *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         description: User ID to edit.
- *         schema:
- *           type: string
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/UserEdit'
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: Leanne Graham
+ *     security:
+ *       - bearerAuth: []
  *     responses:
  *       200:
  *         description: Success, user information updated.
@@ -119,9 +124,25 @@ userRouter.get("/user/:id", [authJwt.verifyToken], getUserInfos);
  *             schema:
  *               type: object
  *               properties:
- *                 message:
- *                   type: string
- *                   example: User updated successfully!
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: User ID.
+ *                       example: 5f9d4f8f5f9d4f8f5f9d4f8f
+ *                     username:
+ *                       type: string
+ *                       description: User username.
+ *                       example: bertrand_test
+ *                     email:
+ *                       type: string
+ *                       description: User email.
+ *                       example: bertrand@test.fr
+ *                     roles:
+ *                       type: array
+ *                       description: User roles.
+ *                       example: ["user", "admin"]
  */
 userRouter.put("/user/:id", [authJwt.verifyToken], editUserInfos);
 
